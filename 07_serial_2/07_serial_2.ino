3eb11674e40aa2948f395e70d06fdf272434fb2d
Volkov Oleksandr <formobileprima@gmail.com>
2021-03-13 21:25:22 +0200

    x

---
+++ 07_serial_2/07_serial_2.ino
@@ -0,0 +1,23 @@
+int readPin = 0;
+int ledPin = 11;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  int readValue = analogRead(readPin);
+
+  int ledPinBrithnes = map(readValue,0,1023, 0,255);
+  String str1 = "readValue: " + String(readValue);
+  String str2 = "readValue: " + String(ledPinBrithnes);
+  Serial.println(str1);
+  Serial.println(str2);
+  analogWrite(ledPin, ledPinBrithnes);
+  delay(200);
+}
